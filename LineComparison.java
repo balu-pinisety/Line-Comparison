64bd0bddbcc9e6fc5f1f4816ceaee68c38cf30ec
balu-pinisetty <balupinisetty@gmail.com>
2021-06-16 05:27:03 +0530

    Added Welcome message of Line Comparison Computation Problem

---
+++ LineComparison.java
@@ -0,0 +1,7 @@
+public class LineComparison {
+
+	public static void main(String[] args) {
+		//Displaying Welcome Message
+		System.out.println("Welcome to Line Comparison Computation Problem\n");
+	}
+}
